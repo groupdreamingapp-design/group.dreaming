6ee0028e27c969110972d182fd07a3daa58303ea
Juan Manuel Correa <group.dreaming.app@gmail.com>
2026-01-02 00:26:30 +0000

    Try fixing this error: `Runtime Error: Failed to load chunk /_next/stati

--- src/app/landing-page-client.tsx
+++ src/app/landing-page-client.tsx
@@ -1,6 +1,5 @@
 'use client';
 
-import { useState } from 'react';
 import { Button } from '@/components/ui/button';
 import { Logo } from '@/components/icons';
 import Link from 'next/link';
@@ -220,5 +219,3 @@ export default function LandingPageClient() {
     </div>
   )
 }
-
-    
